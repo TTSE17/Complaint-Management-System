85c9200a74aef2417f13a6d2e8614eb85d04027a
BesherElMajzoub <88548828+BesherElMajzoub@users.noreply.github.com>
2025-12-10 12:20:06 +0300

    add change to branch admin 2

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff22b422c092991df9b50a47260ee5ed1fc631da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0985eb74057a1636a4ccb418ebc74e2233fcd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Data Access Layer/obj/Debug/net9.0/Data Access Layer.AssemblyInfo.cs
+++ Data Access Layer/obj/Debug/net9.0/Data Access Layer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data Access Layer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff22b422c092991df9b50a47260ee5ed1fc631da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0985eb74057a1636a4ccb418ebc74e2233fcd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data Access Layer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data Access Layer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
